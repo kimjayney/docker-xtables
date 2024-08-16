58436a475a62b868b1437b6b21e611525c9bddfa
jenniecoffee <gim515@naver.com>
2024-08-17 02:56:02 +0900

    FIX latest

--- download_geolite2.sh
+++ download_geolite2.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
-
 TEMPZIP=$(mktemp)
-GEOLITEURL="https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country-CSV&license_key=$LICENSE_KEY&suffix=zip"
-curl $GEOLITEURL > $TEMPZIP
+GEOLITEURL="https://download.maxmind.com/geoip/databases/GeoLite2-Country-CSV/download?suffix=zip"
+curl -L -u $ACCOUNT_ID:$LICENSE_KEY $GEOLITEURL > $TEMPZIP
 unzip -d /tmp -o -j $TEMPZIP '*/GeoLite2-Country-Blocks*'
 rm $TEMPZIP
